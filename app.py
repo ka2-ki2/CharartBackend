420d7da3b13234a6b66d4cff6da2d8bb185078b5
Yadunandan Pillai <ytpillai@pm.me>
2022-04-21 23:53:02 -0400

    Added cors
    Signed-off-by: Yadunandan Pillai <ytpillai@pm.me>

--- app.py
+++ app.py
@@ -16,5 +16,11 @@ app.register_blueprint(users.user_routes, url_prefix="/user")
 app.register_blueprint(bids.bid_routes, url_prefix="/bid")
 
 
-
-
+# put this sippet ahead of all your bluprints
+# blueprint can also be app~~
+@app.after_request 
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    # Other headers can be added here if required
+    return response
